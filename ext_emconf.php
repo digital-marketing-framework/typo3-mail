2a93f80e87388c58cf7d104e2731e9b1473eea69
Stephan Ude <ude@mediatis.de>
2025-05-19 22:54:48 +0200

    MFR-136 dependencies, coding standards

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Digital Marketing Framework - Mail Manager',
+    'title' => 'Anyrel - Mail Manager',
     'description' => 'TYPO3 Mail Manager for DMF',
     'category' => 'be',
     'author_email' => 'info@mediatis.de',
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
         ],
         'conflicts' => [
         ],
