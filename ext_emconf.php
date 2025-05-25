7933cdaedc3676803cf13e3682948a80d4493b6e
s-ude <ude@mediatis.de>
2025-05-26 00:15:39 +0200

    [TASK] TYPO3 13 Update, Backend Module Handling (MFR-136) (#1)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Digital Marketing Framework - Mail Manager',
+    'title' => 'Anyrel - Mail Manager',
     'description' => 'TYPO3 Mail Manager for DMF',
     'category' => 'be',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '12.4.0-13.4.99',
+            'dmf_core' => '3.0.0-3.99.99',
         ],
         'conflicts' => [
         ],
