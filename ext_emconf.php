b67905a8cebcecfdc139fe0ca067bc697ebdbfb0
Stephan Ude <ude@mediatis.de>
2025-05-26 00:12:21 +0200

    MFR-136 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,10 +7,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '1.0.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.4.99',
+            'dmf_core' => '3.0.0-3.99.99',
         ],
         'conflicts' => [
         ],
